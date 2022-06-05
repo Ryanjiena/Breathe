36345163c7a3a451f9fe137a71fec16ead78f1df
Ryanjiena <43134606+Ryanjiena@users.noreply.github.com>
2022-06-05 14:18:01 +0000

    Updated by: runner, at: 2022-06-05 22:17:56

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "lukesampson/scoop-extras:master"
+    [String]$upstream = "ScoopInstaller/Main:master"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
