44fb829323ddc5714cb2b82ca0d5dd53fda7c5cb
Ryanjiena <43134606+Ryanjiena@users.noreply.github.com>
2022-07-15 04:30:35 +0000

    Update by: runner, at: 2022-07-15 04:30:23

--- scripts/clashn/create-scheduled-task.ps1
+++ scripts/clashn/create-scheduled-task.ps1
@@ -4,15 +4,15 @@ $ScheduledTaskName = "clashn-core"
 if ($null -eq (Get-ScheduledTask -TaskName $ScheduledTaskName -ErrorAction SilentlyContinue)) {
     try {
         $ScheduledTaskDescription = "Run $($ScheduledTaskName) at startup"
-        $ScheduledTaskCommand = "Start-Process `"$(scoop prefix clashn-core)\clashN.exe`" -WorkingDirectory `"$(scoop prefix clashn-core)`" -WindowStyle Hidden -Verb RunAs"
-        $ScheduledTaskAction = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-NoProfile -WindowStyle Hidden -command `"$ScheduledTaskCommand`""
+        $Program = "$(scoop prefix clashn-core)/clashN.exe"
+        $ScheduledTaskAction = New-ScheduledTaskAction -Execute "$Program"
         # $SystemSID = New-Object System.Security.Principal.SecurityIdentifier([System.Security.Principal.WellKnownSidType]::LocalSystemSid, $null);
         # $SystemAccountName = $SystemSID.Translate([System.Security.Principal.NTAccount]).Value.ToString();
         # $ScheduledTaskPrincipal = New-ScheduledTaskPrincipal -UserId $SystemAccountName -RunLevel Highest
         $UserId = "$env:USERDOMAIN\$env:USERNAME"
         $ScheduledTaskPrincipal = New-ScheduledTaskPrincipal -UserId $UserId -RunLevel Highest
-        $ScheduledTaskTrigger = New-ScheduledTaskTrigger -AtStartup
-        $ScheduledTaskSettings = New-ScheduledTaskSettingsSet -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 1)
+        $ScheduledTaskTrigger = New-ScheduledTaskTrigger -AtLogOn -User "$env:USERDOMAIN\$env:USERNAME"
+        $ScheduledTaskSettings = New-ScheduledTaskSettingsSet
         $ScheduledTaskObject = New-ScheduledTask -Action $ScheduledTaskAction -Principal $ScheduledTaskPrincipal -Trigger $ScheduledTaskTrigger -Settings $ScheduledTaskSettings -Description $ScheduledTaskDescription
 
         Register-ScheduledTask -TaskName $ScheduledTaskName -InputObject $ScheduledTaskObject | Out-Null
