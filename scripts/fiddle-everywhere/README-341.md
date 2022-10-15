77ca924f5dd36646f9fb976648b85c2ac000711c
Ryanjiena <43134606+Ryanjiena@users.noreply.github.com>
2022-10-15 03:30:27 +0000

    Update by: runner, at: 2022-10-15 03:30:21

--- scripts/fiddle-everywhere/README-341.md
+++
@@ -1,5 +0,0 @@
-# Fiddle Everywhere 3.3.0 Crack
-
-- `main.js`: `resources\app\out`
-- `Fiddler.WebUi.dll` / `FiddlerBackendSDK.dll`: `resources\app\out\WebServer`
-- `main.9c27a5768c5c6da2.js`: `resources\app\out\WebServer\ClientApp\dist`
--- scripts/xyplorer/xyplorer-activate.ps1
+++
@@ -1,3 +0,0 @@
-Copy-Item "$(scoop prefix xyplorer-pro-crack)\XYplorer.Keygen.Activator.exe"  "$(scoop prefix xyplorer-pro-portable)" -Force -ErrorAction 'SilentlyContinue'
-
-sudo Start-Process "$(scoop prefix xyplorer-pro-portable)\XYplorer.Keygen.Activator.exe" -ArgumentList @("/activateinstall") -WorkingDirectory "$(scoop prefix xyplorer-pro-portable)" -WindowStyle Hidden
